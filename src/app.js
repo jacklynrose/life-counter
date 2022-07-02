b5d1d859d1ca3a1100860e8c64db10b03f257e0d
Jacklyn Rose <85480+jacklynrose@users.noreply.github.com>
2022-07-02 11:15:44 +0000

    Life Counter - Creating a game - throws an error if you try to start the game with no player 1 name

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function createGame() {
+  throw new Error("You must provide a player 1 name");
+}
+
+module.exports = {
+  createGame,
+};
--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,12 @@
-describe("Life Counter", () => {});
+const { createGame } = require("../src/app");
+
+describe("Life Counter", () => {
+  describe("Creating a game", () => {
+    it("throws an error if you try to start the game with no player 1 name", () => {
+      const playerOne = undefined;
+      expect(() => {
+        createGame({ playerOne });
+      }).toThrow("You must provide a player 1 name");
+    });
+  });
+});
