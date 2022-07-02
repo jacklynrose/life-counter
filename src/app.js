2e50c55fefc9bcf3d27c5ca5e9bc16f71e86967a
Jacklyn Rose <85480+jacklynrose@users.noreply.github.com>
2022-07-02 11:18:09 +0000

    Life Counter - Creating a game - throws an error if you try to start the game with no player 2 name

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-function createGame() {
-  throw new Error("You must provide a player 1 name");
+function createGame({ playerOne, playerTwo }) {
+  if (!playerOne) {
+    throw new Error("You must provide a player 1 name");
+  }
+  if (!playerTwo) {
+    throw new Error("You must provide a player 2 name");
+  }
 }
 
 module.exports = {
--- test/app.test.js
+++ test/app.test.js
@@ -4,9 +4,18 @@ describe("Life Counter", () => {
   describe("Creating a game", () => {
     it("throws an error if you try to start the game with no player 1 name", () => {
       const playerOne = undefined;
+      const playerTwo = "Yasmin";
       expect(() => {
-        createGame({ playerOne });
+        createGame({ playerOne, playerTwo });
       }).toThrow("You must provide a player 1 name");
+    });
+
+    it("throws an error if you try to start the game with no player 2 name", () => {
+      const playerOne = "Jacklyn";
+      const playerTwo = undefined;
+      expect(() => {
+        createGame({ playerOne, playerTwo });
+      }).toThrow("You must provide a player 2 name");
     });
   });
 });
